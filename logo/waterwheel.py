515e8281b5bb116be0e9b9f0bbfa68d903bc4ebf
C-J-Cundy <chris.j.cundy@gmail.com>
2015-08-18 10:44:16 +0100

    Running logo/waterwheel.py

--- logo/waterwheel.py
+++ logo/waterwheel.py
@@ -2,13 +2,15 @@ from tealight.logo import move, turn
 
 
 def lines(length, number, spacing):
-     for i in range(0,number):
+     for i in range(0,number/2):
         move(length)
         turn(90)
         move(spacing)
         turn(90)
+        move(length)
+        
      turn(90)
      move(spacing*number)
 
-lines(100,100,100)
+lines(100,10,10)
 
