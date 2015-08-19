82f25fa68d8266e82e925732e6e1b1974f28aa6f
C-J-Cundy <chris.j.cundy@gmail.com>
2015-08-19 09:22:20 +0100

    Running art/sketch.py

--- art/sketch.py
+++ art/sketch.py
@@ -18,4 +18,10 @@ def handle_mousemove(x,y,button):
     line(lastx, lasty, x, y)
     lastx = x
     lasty = y
+    
+  elif button == "right":
+    color("red")
+    line(lastx, lasty, x, y)
+    lastx=x
+    lasty=y
   
