eecd9002fdb38928d33471f131435dd6b4981eda
C-J-Cundy <chris.j.cundy@gmail.com>
2015-08-20 12:13:38 +0100

    Running art/minesweeper.py

--- art/minesweeper.py
+++ art/minesweeper.py
@@ -1,1 +1,7 @@
-print "A new file!"
+from tealight.art import (color, line, spot, circle, box, image, text, background)
+
+from tealight.art import (screen_width, screen_height)
+
+def draw_grid(x,y):
+  for i in range(10):
+    print i
