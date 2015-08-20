7148442847de4231f86cc0bd6c54f23231e6350a
C-J-Cundy <chris.j.cundy@gmail.com>
2015-08-20 12:14:26 +0100

    Running art/minesweeper.py

--- art/minesweeper.py
+++ art/minesweeper.py
@@ -4,6 +4,7 @@ from tealight.art import (screen_width, screen_height)
 
 def draw_grid(x,y):
   for i in range(10):
-    print i
+    for j in range(10):
+      box(0,0,100,100)
     
 draw_grid(0,0)
