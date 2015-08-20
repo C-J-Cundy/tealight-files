c098ec9b65610ea93a3e12b99c6b8b25e31d40b5
C-J-Cundy <chris.j.cundy@gmail.com>
2015-08-20 12:22:47 +0100

    Running art/minesweeper.py

--- art/minesweeper.py
+++ art/minesweeper.py
@@ -2,6 +2,10 @@ from tealight.art import (color, line, spot, circle, box, image, text, backgroun
 
 from tealight.art import (screen_width, screen_height)
 
+size=80
+xstart=50
+ystart=50
+
 def draw_grid(x,y,size):
   for i in range(10):
     for j in range(10):
@@ -10,7 +14,9 @@ def draw_grid(x,y,size):
       box(x+size/10+i*size,y+size/10+j*size,size/10*8,size/10*8)
       color("black")
     
-draw_grid(50,50,80)
+draw_grid(xstart,ystart,size)
 
 def handle_mousedown(x,y):
-  print x,",",y
+  boxX=x%size
+  boxY=y%size
+  print boxX,",",boxY
