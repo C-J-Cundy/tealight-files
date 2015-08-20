d93a1631251504a1f6941cc1aea3afbf07e05b14
C-J-Cundy <chris.j.cundy@gmail.com>
2015-08-20 12:27:45 +0100

    Running art/minesweeper.py

--- art/minesweeper.py
+++ art/minesweeper.py
@@ -8,11 +8,6 @@ ystart=50
 
 mines = [[]]
 
-def setup():
-  global mines
-  for i in range(10):
-    for j in range(10):
-      mines[i].append(0)
 
 def draw_grid(x,y,size):
   for i in range(10):
@@ -21,8 +16,16 @@ def draw_grid(x,y,size):
       color("white")
       box(x+size/10+i*size,y+size/10+j*size,size/10*8,size/10*8)
       color("black")
-    
-draw_grid(xstart,ystart,size)
+
+def setup():
+  draw_grid(xstart,ystart,size)
+  global mines
+  for i in range(10):
+    for j in range(10):
+      mines[i].append(0)
+
+setup()
+print mines
 
 def handle_mousedown(x,y):
   global mines
